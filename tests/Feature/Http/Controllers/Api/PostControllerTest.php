bca32b74a0c94644a5fc748155759be5ee08a56d
Alberto Alejandro <alberto196g@gmail.com>
2020-11-14 17:42:35 -0400

    Creando pruebas y realizando la api

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Post;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store()
+    {
+        // $this->withoutExceptionHandling();
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'El post de prueba'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'El post de prueba'])
+            ->assertStatus(201); //OK, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+    }
+
+    public function test_validate_title()
+    {
+        $response = $this->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+
+        $response->assertStatus(422) // Estatus HTTP 422
+            ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $this->withoutExceptionHandling();
+        $post = Post::factory()->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id"); // id = 1
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+                 ->assertJson(['title' => $post->title])
+                 ->assertStatus(200); //Ok
+    }
+
+    public function test_404_show()
+    {
+        $response = $this->json('GET', '/api/posts/1000'); // id = 1
+
+        $response->assertStatus(404); //Ok
+    }
+
+    public function test_updated()
+    {
+        // $this->withoutExceptionHandling();
+        $post = Post::factory()->create();
+
+        $response = $this->json('PUT', "/api/posts/$post->id", [
+            'title' => 'nuevo'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'nuevo'])
+            ->assertStatus(200); //OK, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'nuevo']);
+    }
+
+    public function test_delete()
+    {
+        // $this->withoutExceptionHandling();
+        $post = Post::factory()->create();
+
+        $response = $this->json('DELETE', "/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'nuevo'])
+            ->assertStatus(200); //OK, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'nuevo']);
+    }
+}
